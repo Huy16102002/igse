9e6231d59a40295f32f45bb641f0f87b43fb94c1
Huy Dang <dangquanghuy16102002@gmail.com>
2026-02-25 09:47:07 +0700

    first commit

---
+++ src/main/java/com/example/doanhnghiepathong/DoanhnghiepaThongApplication.java
@@ -0,0 +1,13 @@
+package com.example.doanhnghiepathong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoanhnghiepaThongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoanhnghiepaThongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/doanhnghiepathong/DoanhnghiepaThongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doanhnghiepathong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoanhnghiepaThongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
